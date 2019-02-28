784b8158e7d33fa0cb271e5222a76000e0f74bb8
test <test>
2019-02-27 20:52:49 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Table from './Table';
+import Form from './Form';
 
-class App extends Component {
+class App extends React.Component {
+  state = {
+    characters: []
+  };
+
+  removeCharacter = (index) => {
+    this.setState(
+      {
+        characters: this.state.characters.filter((character, i) => {
+          return i !== index;
+        }
+        )
+      }
+    );
+  }
+
+  handleSubmit = character => {
+    this.setState({characters: [...this.state.characters, character]});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Table
+          characterData={this.state.characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit}/>
       </div>
     );
   }
